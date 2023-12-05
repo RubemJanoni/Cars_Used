14ea573d1f613d8eda8851688cbca9a826a263af
Rubem Janoni <rubemjanoni@gmail.com>
2023-12-05 23:28:31 +0000

    Adjust pp8

--- run.py
+++ run.py
@@ -3,34 +3,31 @@ from google.oauth2.service_account import Credentials
 from termcolor import colored
 
 
-
-
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
-    "https://www.googleapis.com/auth/drive"
-    ]
+    "https://www.googleapis.com/auth/drive",
+]
 
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('cars_used')
-
 cars = SHEET.worksheet('cars')
 data = cars.get_all_values()
-
 
 keys = data[0]
 values = data[1:]
 
 
-#Get a new data from user and add to worksheet
+# Get a new data from user and add to worksheet
+
 def new_car():
     new_car_data = {}
     for key in keys:
         new_car_data[key] = str(input(f'{key}: ')).upper()
 
-    ultimlin = len(values) + 2  # Obtém a próxima linha disponível
+    ultimlin = len(values) + 2
     cars.insert_row(list(new_car_data.values()), index=ultimlin)
 
     print("\nRegistration completed successfully!")
@@ -53,7 +50,10 @@ def get_data_cars(car_model):
     return result
 
 # user answer validation function
+
+
 def error_text(r):
+
     while r != "Y" and r != "N" and r != "YES" and r != "NO":
         r = input("Invalid Input. Try again.\n").upper()
     if r == "YES":
@@ -62,7 +62,9 @@ def error_text(r):
         r = "N"
     return r
 
+
 def delete_data(model):
+
     data = cars.get_all_values()
     for i, row in enumerate(data[1:], start=2):
         if model == row[0]:
@@ -76,6 +78,7 @@ def delete_data(model):
 print(80 * "*")
 print(30 * " " + colored("WELCOME TO CARS USED", 'magenta'))
 print(80 * "*")
+
 while True:
     menu = ""
     print("Menu:\n"
@@ -102,8 +105,10 @@ while True:
 
             if menu == "1":
                 print(colored("Get a car NOW!", 'green'))
-                print(colored("Search a car and get the specs and prices.\n", 'cyan'))
-                car_model = input("Type a brand and model, like BMW Z4:\n").upper()
+                print(colored(
+                    "Search a car and get the specs and prices.\n", 'cyan'))
+                car_model = input(
+                    "Type a brand and model, like BMW Z4:\n").upper()
                 new_data = get_data_cars(car_model)
                 if not new_data:
                     print(colored("CAR NOT FOUND", 'red'))
@@ -111,7 +116,6 @@ while True:
                     menu = input("1 - Search a new car\n"
                                  "2 - Register a new car\n"
                                  "3 - Back to Menu\n")
-                                 
                     while menu != "1" and menu != "2" and menu != "3":
                         resposta = input("Invalid input.\n")
                     if menu == "3":
@@ -120,8 +124,12 @@ while True:
                 else:
                     print(colored("Specifications:\n", 'green'))
                     for key, value in new_data[0].items():
-                        print(colored(f"{key.capitalize()}: {value}\n", 'blue'))
-                    resposta = input(colored("Would you like to try another model? Y/N?\n", 'magenta'))
+                        print(colored(
+                            f"{key.capitalize()}: {value}\n", 'blue'))
+                    resposta = input(
+                        colored(
+                            "Would you like to try another model? Y/N?\n",
+                            'magenta'))
                     resposta = error_text(resposta.upper())
                     if resposta.lower() != "y":
                         print()
@@ -134,7 +142,8 @@ while True:
                     values = data[1:]
                     print("Enter vehicle specifications.")
                     new_car()
-                    resposta = input("Would you like a new register? Y/N?\n").upper()
+                    resposta = input(
+                        "Would you like a new register? Y/N?\n").upper()
                     resposta = error_text(resposta)
                     if resposta.lower() != "y":
                         menu = ""
@@ -147,13 +156,15 @@ while True:
                 while True:
                     data = cars.get_all_values()
                     values = data[1:]
-                    car_model = input("Type a brand and model, such as BMW Z4:\n").upper()
+                    car_model = input(
+                        "Type a brand and model, such as BMW Z4:\n").upper()
                     achou = delete_data(car_model)
                     if achou is False:
                         print(colored("Car not found", 'red'))
-                        resposta = input("1 - Delete another car\n"
-                                     "2 - Back to menu\n"
-                                     "Choose an option above: ")
+                        resposta = input(
+                            "1 - Delete another car\n"
+                            "2 - Back to menu\n"
+                            "Choose an option above: ")
                         while resposta != "1" and resposta != "2":
                             resposta = input("Invalid Input, try again.\n")
                         if resposta == "1":
@@ -163,7 +174,10 @@ while True:
                             menu = ""
                             break
                     else:
-                        resposta = input(colored("Would you like to try another model? Y/N?\n", 'magenta'))
+                        resposta = input(
+                            colored(
+                                "Would you like to try another model? Y/N?\n",
+                                'magenta'))
                         resposta = error_text(resposta.upper())
                         if resposta.lower() != "y":
                             menu = ""
